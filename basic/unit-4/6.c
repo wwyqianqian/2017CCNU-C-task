f62ee19f7b9f04e9703e2f9fa560a3401c61bf65
wwyqianqian <1208331933@qq.com>
2017-03-04 01:18:11 +0800

    update webhooks

--- basic/unit-4/6.c
+++ basic/unit-4/6.c
@@ -14,6 +14,5 @@ void main()
   m = (r % 3600) / 60;    //minutes
   s = (r % 3600) % 60;    //seconds
 
-  printf("Have passed %d days, current time is %02d:%02d:%02d\n", d, h, m, s);
-  
+  printf("Have passed %d days, current time is %02d:%02d:%02d\n", d, h, m, s);  
 }
