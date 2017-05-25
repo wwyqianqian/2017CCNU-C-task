da16bfcb8614a329e9e294eba13c19eb33c6d1d3
wwyqianqian <1208331933@qq.com>
2017-05-25 22:57:10 +0800

    class task

--- andian.c
+++ andian.c
@@ -1,3 +1,65 @@
-鞍点。
-统计字符串个数
-输入一组数，最大与最大的与第一个元素交换，最小的与最后一个元素交换，然后输出这组数。
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<malloc.h>
+
+typedef struct student{
+  char num[10];
+  char nane[20];
+  char class[20];
+  float score;
+
+  struct student *next;
+
+}student;
+
+int main(void){
+  FILE *fi, *fo;
+  fi = fopen("./input.data", "r");
+  fo = fopen("./input.data", "w+");
+
+  student *head = (student*)malloc(sizeof student);
+
+  if(head = NULL){
+    printf("none");
+    return;
+  }
+
+
+  strcpy(head->name," ");
+
+  //next
+  current->next = (student*)malloc(sizeof student);
+  current = current->next;
+
+
+  printf("After sorted: \n" );
+  fi = head->next;
+  while (fi != NULL) {
+    printf("%d", p->score);
+    p = p->next;
+  }
+}
+
+
+void cpscore(student **stu){
+  student *current = NULL;
+  student *big = NULL;
+  student *next = NULL;
+
+  current = *stu;
+  big = *stu;
+  next = *sru;
+
+  while (current != NULL && current->next != NULL) {
+    while (next != NULL) {
+      if (next->score > big->score) {
+        big = next;
+      }
+      //exchange
+    }
+  }
+    *current ^= *big;
+    *big ^= *current;
+    *current ^= *big;
+}
